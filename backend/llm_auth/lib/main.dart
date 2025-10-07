34261bca31937526329bf4142ca306479c7b5748
Pepe Diedrich <pepediedrich@proton.me>
2025-10-07 22:11:08 +0200

    feat: initial implementation of main.dart as library entrypoint for inkpadu_llm_auth package

---
+++ backend/llm_auth/lib/main.dart
@@ -0,0 +1,5 @@
+// Library entrypoint for the inkpadu_llm_auth package.
+// Exposes the Appwrite-compatible function `main(req, res)` so that
+// other tools can import `package:inkpadu_llm_auth/main.dart`.
+
+export '../main.dart';
